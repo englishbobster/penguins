dfeb0cf46e0f700503711ab5f3168bcd1bf928c5
Stuart Osborne <stuart.osborne@transmode.se>
2016-09-12 20:14:09 +0200

    Refactored Hexagon module. Now standalone.

--- src/Hexagon.elm
+++ src/Hexagon.elm
@@ -1,9 +1,9 @@
 module Hexagon exposing (..)
 
-import String exposing (..)
-import Html exposing (..)
-import Svg exposing (..)
-import Svg.Attributes exposing (..)
+import String exposing (join)
+import Html exposing (Html, div)
+import Svg exposing (Svg, polygon)
+import Svg.Attributes exposing (points)
 import Html.Attributes exposing (class, rel, href)
 
 
@@ -11,44 +11,38 @@ type alias Coord =
     (Float, Float)
 
 
+drawHexagon : Coord -> Float -> String -> String -> Html Cmd
+drawHexagon center size frontColour backColour =
+    div [] [ css "../flip.css"
+           , hexagonFlipper center size frontColour backColour
+           ]
+
+
 css : String -> Html Cmd
 css path =
   Html.node "link" [ rel "stylesheet", href path ] []
 
 
-view : Html Cmd
-view =
-    div [] [ css "../flip.css"
-           , flipper
-           ]
-
-flipper : Html Cmd
-flipper =
+hexagonFlipper : Coord -> Float -> String -> String -> Html Cmd
+hexagonFlipper center size frontColour backColour =
     div [ Html.Attributes.class "flip-container" ]
         [ div [ Html.Attributes.class "flipper" ]
               [ div [ Html.Attributes.class "front" ]
-                    [ hexagon (60, 60) 50 "blue" ]
+                    [ hexagonFace center size frontColour ]
               , div [ Html.Attributes.class "back" ]
-                    [ hexagon (60, 60) 50 "red" ]
+                    [ hexagonFace center size backColour ]
               ]
         ]
+
+
+hexagonFace : Coord -> Float -> String -> Svg Cmd
+hexagonFace center size colour =
+    Svg.svg [] [ polygon [ points ( hexagonPoints center size ), hexColour colour ] [] ]
+
 
 hexColour: String -> Svg.Attribute msg
 hexColour colour =
     Svg.Attributes.style ( "fill:" ++ colour ++ ";stroke:black;stroke-width:3" )
-
-
-hexagon : Coord -> Float -> String -> Svg Cmd
-hexagon center size colour =
-    Svg.svg [] [ polygon [points ( hexagonPoints center size ), hexColour colour] [] ]
-
-
-hexCorner : Coord -> Float -> Int -> Coord
-hexCorner center size cornerIndex =
-    let
-        angleRad = Basics.degrees (60 * (Basics.toFloat cornerIndex) + 60)
-    in
-        (fst center + size * (cos angleRad), snd center + size *(sin angleRad))
 
 
 hexagonPoints : Coord -> Float -> String
@@ -59,6 +53,9 @@ hexagonPoints center size =
             |> join ","
 
 
-main : Html Cmd
-main =
-    view
+hexCorner : Coord -> Float -> Int -> Coord
+hexCorner center size cornerIndex =
+    let
+        angleRad = Basics.degrees (60 * (Basics.toFloat cornerIndex) + 60)
+    in
+        (Basics.fst center + size * (cos angleRad), Basics.snd center + size *(sin angleRad))
